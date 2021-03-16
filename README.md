e04ea848081cd502fb2a99f46394bc1a2b1b77cd
maudevjr <mauriciojr.dev@gmail.com>
2021-03-16 17:12:31 -0300

    - Adição do pacote ejs - Nova função para fazer upload dos arquivos kml - Correção de bugs

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
 # KML-Coordinate-Extractor
 Uma aplicação para extrair coordenadas geográficas de arquivos KML.
 
-Em breve, dicas de como usar.
+<b>Como usar:</b>
+<ol>
+    <li>
+        Renomeie seu arquivo KML para file.kml.
+    </li>
+    <li>
+        Cole o arquivo KML no seguinte diretório <i>src/public</i>.
+    </li>
+    <li>
+        Abra o terminal na raiz da pasta aplicação e digite "npm start" sem aspas, logo em seguida aperte enter.
+    </li>
+    <li>
+        Acesse via browser o seguinte URL localhost:3000/home, e clique no botão executar.
+    </li>
+
+</ol>
+    
--- main.js
+++ main.js
@@ -1,18 +1,20 @@
 const express = require('express');
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-const dotenv = require('dotenv')
-
-dotenv.config()
+const multer = require('multer')
+const upload = multer({ dest:'src/public'})
 
 app.set('view engine', 'ejs');
+
+app.use('/static', express.static(__dirname + '/src/public'))
 
 app.get('/home', (req, res) => {
     res.render('../src/views/index')
 })
 
-app.use('/static', express.static(__dirname + '/src/public'))
+app.post('/file', upload.single('file'),
+(req, res)=>res.send('<p>Enviado</p>'))
 
 app.listen(port, () => {
     console.log(`Running at port ${port}`)
